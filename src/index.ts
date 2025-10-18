22f6fbc790cfed7d696a69504284d151d6fd655d
coderRaj07 <12131501.edu@gmail.com>
2025-10-18 21:35:11 +0530

    [master] feat(CORS): CORS added and envs updates

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,18 @@
 import "dotenv/config";
+import cors from "cors";
 import express from "express";
 
 import { getProperties } from "./use-cases/getProperties";
 
 const app = express();
+app.use(
+    cors({
+      origin: [process.env.ALLOWED_DOMAIN!] as string[],
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
+
 const port = process.env.PORT || 5000;
 
 app.get("/", (_req, res) => res.send("Warden Weather Test: OK"));
